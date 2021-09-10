0baed1b7f3a286423682d763474a9fcc0ca4c545
smcha <wlauswlaus@hanmail.net>
2021-09-10 16:53:57 +0900

    박스오피스 완료

--- script/main.js
+++ script/main.js
@@ -1,5 +1,43 @@
 $(function(){
+    ad();
+    modal();
+    movieExplorer();
+    setInterval(mouseScroll, 1500);
+
+})
+function mouseScroll() {
+    $("#mouse").animate({
+        bottom: "-40"
+    }, "swing", function(){
+        $("#mouse").animate({
+            bottom: "-35"
+        });
+    })
+}
+function movieExplorer() {
+    $("input[type=image]").click(function(){
+        var keyword = $("input[type=text]").val();
+        if(!keyword) {
+            alert("영화명을 입력하셔야 합니다");
+        } else {
+            console.log(keyword);
+            location.href="https://www.megabox.co.kr/movie?searchText="+keyword;
+        }
+    });
+}
+function modal() {
+    $(".favorite").click(function(){
+        $("#modal").show();
+    })
+    $("#close_x").click(function(){
+        $("#modal").hide();
+    })
+    $("#close_ok").click(function(){
+        $("#modal").hide();
+    })
+}
+function ad() {
     $(".ad_close").click(function(){
         $(".ad").hide();
     })
-})
+}
